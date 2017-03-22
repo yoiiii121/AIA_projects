f39713e905ba598ce2da7d740100c372753b5ef6
yoiiii121 <yoiiii121@gmail.com>
2017-03-22 01:03:46 +0100

    fixed errors

--- AIA_text_clasification/clustering.py
+++ AIA_text_clasification/clustering.py
@@ -1,96 +1,119 @@
 import random as ra
-import math as ma
+
 import numpy as np
-from nltk.corpus import stopwords
 from sklearn.datasets import load_iris
 
 data = load_iris()
+data2 = [[51], [43], [62], [64], [45], [42], [46], [45], [45], [62], [47], [52], [64], [51], [65], [48], [49],
+         [46], [64], [51], [52], [62], [49], [48], [62], [43], [40], [48], [64], [51], [63], [43], [65], [66],
+         [65], [46], [39], [62], [64], [52], [63], [64], [48], [64], [48], [51], [48], [64], [42], [48], [41]]
 
 
-#stopwords.download()
-#stw = [ word for word in palabras if word not in stopwords.words('spanish') ]
-def distance_euclidean(data,mean_value):
+# stopwords.download()
+# stw = [ word for word in palabras if word not in stopwords.words('spanish') ]
+def distance_euclidean(data, mean_value):
     total = 0
-    for i in range(0,len(data)):
-        total += (data[i]-mean_value[i])**2
+    for i in range(0, len(data)):
+        total += (data[i] - mean_value[i]) ** 2
     return np.sqrt(total)
 
-def distance_manhattan(data,mean_value):
+
+def distance_manhattan(data, mean_value):
     total = 0
-    for i in range(0,len(data)):
-        total += np.fabs(data[i]-mean_value[i])
+    for i in range(0, len(data)):
+        total += np.fabs(data[i] - mean_value[i])
     return total
 
-def distance_maximo(data,mean_value):
+
+def distance_maximo(data, mean_value):
     total = 0
     for i in range(0, len(data)):
         aux = np.fabs(data[i] - mean_value[i])
-        if total > aux:
+        if total < aux:
             total = aux
     return total
 
 
-def distance_hamming(data,mean_value):
-    return (np.array(data)!=np.array(mean_value)).mean()
+def distance_hamming(data, mean_value):
+    return (np.array(data) != np.array(mean_value)).mean()
 
-#print(distance_euclidea(data['data'],np.array([1.,2.,3.,4.])))
-data_values = np.array([[1., 2., 4.],[2.,3.,3.]])
-
-
-mean_value = np.array([2., 5., 2.])
-
-#print(distance_euclidean(data_values[0],mean_value))
-#print(distance_manhattan(data_values[0],mean_value))
 
 print("Number of elements: {}".format(len(data['data'])))
 
-def k_my_kmedias(k,data,distance):
+
+def mean(list_elem):
+    total = 0
+    for i in list_elem:
+        total += i
+    return total / len(list_elem)
+
+
+def k_my_kmedias(k, data, distance, iterations):
     centroids_list = []
     repeat = True
     group = 0
 
     # Inicializate
+    l = []
     size = len(data) - 1
     for i in range(k):
         centroids_list_ = []
         for j in range(len(data[0])):
-            centroids_list_.append(data[ra.randint(0,size)][j])
+            aux = ra.randint(0, size)
+            boolean = True
+            while (boolean):
+                aux = ra.randint(0, size)
+                if aux not in l:
+                    l.append(aux)
+                    boolean = False
+            centroids_list_.append(round(float(data[aux][j]), 1))
         centroids_list.append(centroids_list_)
-
     # Repeat
     cont = 0
-    while(repeat):
-        cont +=1
+    while (repeat):
         fill_list = []
+        for j in range(k):
+            fill_list.append([])
         for i in range(len(data)):
             minimum = 9999.
             for j in range(k):
-               aux = distance(data[i],centroids_list[j])
-               if minimum > aux:
-                   minimum = aux
-                   group = j
-            fill_list.append([data[i],group])
+
+                aux = distance(data[i], centroids_list[j])
+                if minimum > aux:
+                    minimum = aux
+                    group = j
+            fill_list[group].extend(data[i])
         centroids_list2 = []
         for i in range(k):
             centroids_list2_ = []
-            for j in range(len(fill_list[0][0])):
-                classify_ = []
-                for k1 in range(len(fill_list)):
-                    if fill_list[k1][1] == i:
-                        classify_.append(fill_list[k1][0][j])
-                if classify_ != []:
-                    centroids_list2_.append(np.mean(classify_))
-                else:
-                    centroids_list2_.append(0.)
-            centroids_list2.append(centroids_list2_)
+            for k1 in range(len(data[0])):
+                l = []
 
+                for j in range(0, len(fill_list[i]), len(data[0]) + k1):
+                    l.append(round(fill_list[i][j], 1))
+                value = 0.0
+                if l:
+                    value = np.mean(l)
+                centroids_list2_.append(round(float(value), 1))
+            centroids_list2.append(centroids_list2_)
+        if iterations - 1 == cont:
+            repeat = False
         if centroids_list == centroids_list2:
             repeat = False
         centroids_list = centroids_list2.copy()
+
+        cont += 1
+
     return centroids_list
 
-k = 10
-print("Euclidean distance: {}".format(k_my_kmedias(k,data['data'],distance_euclidean)))
-print("Manhattan distance: {}".format(k_my_kmedias(k,data['data'],distance_manhattan)))
-print("Maximo distance: {}".format(k_my_kmedias(k,data['data'],distance_maximo)))
-print("Hamming distance: {}".format(k_my_kmedias(k,data['data'],distance_hamming)))
+
+k = 2
+iterations = 3
+print("Euclidean distance: {}".format(k_my_kmedias(k, data2, distance_euclidean, iterations)))
+
+k = 3
+iterations = 6
+print("Euclidean distance: {}".format(k_my_kmedias(k, data['data'][:, 0:3], distance_euclidean, iterations)))
+print("Manhattan distance: {}".format(k_my_kmedias(k, data['data'][:, 0:3], distance_manhattan, iterations)))
+print("Maximo distance: {}".format(k_my_kmedias(k, data['data'][:, 0:3], distance_maximo, iterations)))
+print("Hamming distance: {}".format(k_my_kmedias(k, data['data'][:, 0:3], distance_hamming, iterations)))
