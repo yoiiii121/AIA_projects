9c339ce6d0ca7eac47635d4e0373cb4111eddc80
yoiiii121 <yoiiii121@gmail.com>
2017-03-24 14:09:16 +0100

    incomplete task 2

--- AIA_text_clasification/clustering.py
+++ AIA_text_clasification/clustering.py
@@ -2,6 +2,123 @@ import random as ra
 
 import numpy as np
 from sklearn.datasets import load_iris
+import scipy.sparse as sc
+
+
+from nltk.corpus import stopwords
+import re
+import unicodedata
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+vectorizer = TfidfVectorizer(min_df=1,smooth_idf = False,strip_accents="ascii",lowercase=True)
+corpus = ["Juan quiere comprar un coche. Ana no quiere comprar ningún coche"]
+print(vectorizer)
+
+print("Using simple way")
+#Simple model
+vectorizer.norm = None
+#vectorizer.use_idf = False
+vectorizer_fit = vectorizer.fit_transform(corpus)
+print(vectorizer_fit)
+print(vectorizer.get_feature_names())
+
+#Simple model with normalized euclidean distance
+vectorizer.norm ='l2'
+vectorizer.use_idf = True
+vectorizer.smooth_idf = True
+vectorizer.sublinear_tf = False
+vectorizer_fit = vectorizer.fit_transform(corpus)
+print(vectorizer_fit)
+print(vectorizer.get_feature_names())
+
+#Simple model with normalized coseno distance
+vectorizer.norm ='l1'
+vectorizer_fit = vectorizer.fit_transform(corpus)
+print(vectorizer_fit)
+print(vectorizer.get_feature_names())
+
+
+
+print()
+print()
+#Using stop words
+print("Using stop words")
+vectorizer.stop_words =stopwords.words('spanish')
+
+
+#Simple model
+vectorizer.norm = None
+#vectorizer.use_idf = False
+vectorizer_fit = vectorizer.fit_transform(corpus)
+print(vectorizer_fit)
+print(vectorizer.get_feature_names())
+
+#Simple model with normalized euclidean distance
+vectorizer.norm ='l2'
+vectorizer.use_idf = True
+vectorizer.smooth_idf = True
+vectorizer.sublinear_tf = False
+vectorizer_fit = vectorizer.fit_transform(corpus)
+print(vectorizer_fit)
+print(vectorizer.get_feature_names())
+
+#Simple model with normalized coseno distance
+vectorizer.norm ='l1'
+vectorizer_fit = vectorizer.fit_transform(corpus)
+print(vectorizer_fit)
+print(vectorizer.get_feature_names())
+
+print()
+print("TO-DO: tomando raices de palabras")
+print("TO-DO: tomando raices de palabras")
+print("TO-DO: tomando raices de palabras")
+
+#Using tfidf
+print()
+print()
+print("Using tfidf")
+vectorizer.tfidf =True
+
+
+#Simple model
+vectorizer.norm = None
+#vectorizer.use_idf = False
+vectorizer_fit = vectorizer.fit_transform(corpus)
+print(vectorizer_fit)
+print(vectorizer.get_feature_names())
+
+#Simple model with normalized euclidean distance
+vectorizer.norm ='l2'
+vectorizer.use_idf = True
+vectorizer.smooth_idf = True
+vectorizer.sublinear_tf = False
+vectorizer_fit = vectorizer.fit_transform(corpus)
+print(vectorizer_fit)
+print(vectorizer.get_feature_names())
+
+#Simple model with normalized coseno distance
+vectorizer.norm ='l1'
+vectorizer_fit = vectorizer.fit_transform(corpus)
+print(vectorizer_fit)
+print(vectorizer.get_feature_names())
+
+
+#def elimina_tildes(s):
+#    return ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
+
+
+#text = re.findall(r"[A-Za-zÁ-Úá-ú]+", elimina_tildes(str(corpus)))
+
+# In the python console, you need to input nltk.download()
+#print(stopwords.words('spanish'))
+#stw = [ word for word in text if word not in stopwords.words('spanish') ]
+#print(stw)
+
+print()
+
+
+
+
 
 data = load_iris()
 data2 = [[51], [43], [62], [64], [45], [42], [46], [45], [45], [62], [47], [52], [64], [51], [65], [48], [49],
@@ -9,8 +126,6 @@ data2 = [[51], [43], [62], [64], [45], [42], [46], [45], [45], [62], [47], [52],
          [65], [46], [39], [62], [64], [52], [63], [64], [48], [64], [48], [51], [48], [64], [42], [48], [41]]
 
 
-# stopwords.download()
-# stw = [ word for word in palabras if word not in stopwords.words('spanish') ]
 def distance_euclidean(data, mean_value):
     total = 0
     for i in range(0, len(data)):
@@ -117,3 +232,4 @@ print("Euclidean distance: {}".format(k_my_kmedias(k, data['data'][:, 0:3], dist
 print("Manhattan distance: {}".format(k_my_kmedias(k, data['data'][:, 0:3], distance_manhattan, iterations)))
 print("Maximo distance: {}".format(k_my_kmedias(k, data['data'][:, 0:3], distance_maximo, iterations)))
 print("Hamming distance: {}".format(k_my_kmedias(k, data['data'][:, 0:3], distance_hamming, iterations)))
+
